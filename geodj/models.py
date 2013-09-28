f0041b913fe47d458acea436b77a353bbed1a84b
Peter Graham <peterghm@gmail.com>
2013-09-27 21:31:28 -0700

    Fix country/artist models

--- geodj/models.py
+++ geodj/models.py
@@ -1,20 +1,10 @@
 from django.db import models
 
 class Country(models.Model):
-  # def __init__(self, name, iso_code):
-  #   self.name = name
-  #   self.iso_code = iso_code
-  
-  name = models.CharField(max_length = 200)
-  iso_code = models.CharField(max_length=3)
-
+    name = models.TextField()
+    iso_code = models.CharField(max_length=2)
 
 class Artist(models.Model):
-  # def __init__(self, name, mbid, country_id):
-  #   self.name = name
-  #   self.mbid = mbid
-  #   self.country_id = country_id
-
-  name = models.CharField(max_length = 200)
-  mbid = models.TextField()
-  country_id = models.TextField()
+    name = models.TextField()
+    mbid = models.TextField()
+    country = models.ForeignKey(Country)
