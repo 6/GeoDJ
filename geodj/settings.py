7aa21653523a55d69f5c4c0231fe33735d9cc138
Peter Graham <peterghm@gmail.com>
2013-09-28 09:53:51 -0700

    enable django admin

--- geodj/settings.py
+++ geodj/settings.py
@@ -133,9 +133,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'geodj',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
