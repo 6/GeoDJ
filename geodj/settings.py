540b75d0422df297b64cd28980d49030b1dba6fe
Peter Graham <peterghm@gmail.com>
2013-09-27 18:44:54 -0700

    configure static files

--- geodj/settings.py
+++ geodj/settings.py
@@ -3,6 +3,8 @@ import os
 os.environ.setdefault("APP_ENV", "development")
 os.environ.setdefault("SECRET_KEY", "^uhrm48x9y=1f&+$bg=oc(#23mp0*g5k%8+si9tdz7&4_xk&lf")
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = os.environ['APP_ENV'] != 'production'
 TEMPLATE_DEBUG = DEBUG
 
@@ -66,7 +68,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -74,9 +76,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
