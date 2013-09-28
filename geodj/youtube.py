912ec83516b891331ddf24d6cc4dfcd8fe576ae9
Peter Graham <peterghm@gmail.com>
2013-09-28 16:34:42 -0700

    Remove unidecode

--- geodj/youtube.py
+++ geodj/youtube.py
@@ -1,6 +1,5 @@
 from gdata.youtube.service import YouTubeService, YouTubeVideoQuery
 from django.utils.encoding import smart_str
-from unidecode import unidecode
 
 class YoutubeMusic:
     def __init__(self):
@@ -30,6 +29,6 @@ class YoutubeMusic:
             return False
         if duration < (2 * 60) or duration > (9 * 60):
             return False
-        if unidecode(artist).lower() not in unidecode(smart_str(entry.media.title.text)).lower():
+        if artist.lower() not in smart_str(entry.media.title.text).lower():
             return False
         return True
