a50485b6466d2c4993cd67c76e6d10da37a46f09
Peter Graham <peterghm@gmail.com>
2013-09-27 22:29:39 -0700

    add uniqueness constraints on ISO code and musicbrainz ID

--- geodj/models.py
+++ geodj/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Country(models.Model):
     name = models.TextField()
-    iso_code = models.CharField(max_length=2)
+    iso_code = models.CharField(max_length=2, unique=True)
 
 class Artist(models.Model):
     name = models.TextField()
-    mbid = models.TextField()
+    mbid = models.TextField(unique=True)
     country = models.ForeignKey(Country)
