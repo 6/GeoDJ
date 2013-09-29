1eb2d8a4c51e54ef95c93f40662971ada8178093
Peter Graham <peterghm@gmail.com>
2013-09-28 22:50:46 -0700

    Add continent to admin panel

--- geodj/admin.py
+++ geodj/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from geodj.models import Country, Artist
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'iso_code')
+    list_display = ('name', 'iso_code', 'continent')
 
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ('name', 'mbid', 'country')
