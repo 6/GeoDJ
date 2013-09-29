c463b7e4ee63a5e1e93757af3b037e33636125fd
Peter Graham <peterghm@gmail.com>
2013-09-29 08:36:44 -0700

    Extract getting artist details into separate method

--- geodj/musicbrainz.py
+++ geodj/musicbrainz.py
@@ -4,18 +4,25 @@ class MusicBrainz:
     def __init__(self):
         musicbrainzngs.set_useragent("GeoDJ", "0.0.1", "https://github.com/6/djangodash2013")
 
-    def is_artist_from_country(self, musicbrainz_id, country_code):
+    def get_artist(self, musicbrainz_id, includes=[]):
         if musicbrainz_id is None or musicbrainz_id.strip() == "":
-            return False
+            return None
         try:
-            result = musicbrainzngs.get_artist_by_id(musicbrainz_id)
+            result = musicbrainzngs.get_artist_by_id(musicbrainz_id, includes=includes)
         except musicbrainzngs.WebServiceError as exc:
             print "Something went wrong with the request: %s" % exc
-            return False
+            return None
         except musicbrainzngs.MusicBrainzError as exc:
             print "Something went wrong with MusicBrainz: %s" % exc
-            return False
+            return None
         else:
-            if 'artist' in result and 'country' in result['artist']:
-                return result['artist']['country'] == country_code
+            return result
+
+    def is_artist_from_country(self, musicbrainz_id, country_code):
+        result = self.get_artist(musicbrainz_id)
+        if result is None:
             return False
+
+        if 'artist' in result and 'country' in result['artist']:
+            return result['artist']['country'] == country_code
+        return False
