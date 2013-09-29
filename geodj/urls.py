1707e19dd359929440b86af504f4f7dfd49559df
Peter Graham <peterghm@gmail.com>
2013-09-29 07:15:55 -0700

    Add basic favicon

--- geodj/urls.py
+++ geodj/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from geodj import views
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^countries/(?P<country_id>\d+)/videos$', views.videos, name='videos'),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 )
